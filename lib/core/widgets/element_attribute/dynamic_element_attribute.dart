c3b3297dd861848c9a11b62d7bda32116e1a47f6
dkukaj <ditmir.kukaj@gmail.com>
2025-09-19 14:44:32 +0200

    remove

--- lib/core/widgets/element_attribute/dynamic_element_attribute.dart
+++ lib/core/widgets/element_attribute/dynamic_element_attribute.dart
@@ -17,7 +17,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:uuid/uuid.dart';
 
 class DynamicElementAttribute extends StatefulWidget {
   final List<FieldSchema> schema;
